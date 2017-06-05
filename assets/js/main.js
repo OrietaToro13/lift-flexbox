7adc9253a093c9713cf8f6f1ba06bf5366ef64ae
Orieta Toro <orieta.toro@gmail.com>
2017-06-04 23:13:56 -0400

    2 repositorio inicio

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>lyft-flexbox</title>
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
